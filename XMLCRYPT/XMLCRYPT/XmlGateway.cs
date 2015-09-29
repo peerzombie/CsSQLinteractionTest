201ea4cd1fcd6196417c32910e0964df359a6ea8
TheLazyAdmin <falschich@gmail.com>
2015-09-29 15:20:02 +0200

    XMLCRYPT Introduction

---
+++ XMLCRYPT/XMLCRYPT/XmlGateway.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml;
+
+namespace XMLCRYPT
+{
+    public class XmlGateway
+    {
+        private string filepath = "data/data.xml";
+        public XmlGateway(string file)
+        {
+            filepath = file;
+        }
+
+        public void CreateRoot()
+        {
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load(filepath);
+            XmlElement elem = xdoc.CreateElement("root");
+            elem.SetAttribute("MODEL", "STANDARD");
+            XmlCDataSection cdata = xdoc.CreateCDataSection("");
+            elem.AppendChild(cdata);
+            xdoc.AppendChild(elem);
+            xdoc.Save(filepath);
+        }
+
+        public void AddEnc(string name, string hash, string inc, DateTime created)
+        {
+            
+        }
+    }
+}
---
+++ XMLCRYPT/XMLCRYPT/main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using cryptography;
+
+namespace XMLCRYPT
+{
+    public partial class main : Form
+    {
+        private crypt c = new crypt(1);
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
